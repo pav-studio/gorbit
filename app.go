9f8a884ad36077df7dff05960b2e463379d1533e
Tanishq Dhote <119649887+ptsdpatient@users.noreply.github.com>
2026-07-20 10:26:34 +0530

    initial commit

---
+++ app.go
@@ -0,0 +1,10 @@
+package gonode
+import ("fmt")
+
+func Add(num1 int, num2 int) int {
+	return num1 + num2;
+}
+
+func main() {
+	fmt.Print("Hello world")
+}
---
+++ example/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"gonode"
+)
+
+func main() {
+	fmt.Print(gonode.Add(4,6))
+}
